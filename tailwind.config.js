6e8bb78ec8c0d356676af981bbcf69b6b5bb414c
Filip Kilichowski <88538522+Lambard2@users.noreply.github.com>
2024-04-12 11:35:06 +0200

    reszta strony

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,15 @@ module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
     extend: {
+      fontFamily:{
+        montserrat: ["Montserrat", "sans-serif"]
+      },
       backgroundOpacity: ['active'],
       colors: {
         "primary": "#ffcc00",
         "secondary": "#3a3a39",
+        "footer": "#212121",
+        "footer2": "#141414"
       },
       backgroundImage:{
         "main": "url('../assets/kolor_tlo.jpg')"
